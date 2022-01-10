a036bc4bf59222044c78d2e8d0c5e55108025be1
SrimithraBingi <bingisrimithra@gmail.com>
2022-01-10 15:56:53 -0800

    Done.

--- src/CalcC.cs
+++ src/CalcC.cs
@@ -38,8 +38,68 @@ namespace CalcC
                 // If you get stuck, think about what the
                 // code would look like in C# and use
                 // sharplab.io to see what the CIL would be.
-                
-                throw new NotImplementedException();
+                switch (tokenType)
+                {
+                    case Number:
+                        cil += $@"
+                        ldloc.0
+                        ldc.i4.s {token}
+                        callvirt instance void class [System.Collections]System.Collections.Generic.Stack`1<int32>::Push(!0)
+                        ";
+
+                        break;
+
+                    case BinaryOperator:
+                        var instruction = token switch
+                        {
+                            "+" => "add",
+                            "-" => "sub",
+                            "*" => "mul",
+                            "/" => "div",
+                            "%" => "rem",
+                            _ => throw new InvalidOperationException(nameof(token)),
+                        };
+                        cil += $@"
+                        ldloc.0
+                        ldloc.0
+                        callvirt instance !0 class [System.Collections]System.Collections.Generic.Stack`1<int32>::Pop()
+                        ldloc.0
+                        callvirt instance !0 class [System.Collections]System.Collections.Generic.Stack`1<int32>::Pop()
+                        {instruction}
+                        callvirt instance void class [System.Collections]System.Collections.Generic.Stack`1<int32>::Push(!0)
+                        ";
+
+                        break;
+
+                    case StoreInstruction:
+                        var register = token[1];
+                        cil += $@"
+                        ldloc.1
+                        ldc.i4.s {(int)register}
+                        ldloc.0
+                        callvirt instance !0 class [System.Collections]System.Collections.Generic.Stack`1<int32>::Pop()
+                        callvirt instance void class [System.Private.CoreLib]System.Collections.Generic.Dictionary`2<char, int32>::set_Item(!0, !1)
+                        ";
+
+                        break;
+
+                    case RetrieveInstruction:
+                        register = token[1];
+                        cil += $@"
+                        ldloc.0
+                        ldloc.1
+                        ldc.i4.s {(int)register}
+                        callvirt instance !1 class [System.Private.CoreLib]System.Collections.Generic.Dictionary`2<char, int32>::get_Item(!0)
+                        callvirt instance void class [System.Collections]System.Collections.Generic.Stack`1<int32>::Push(!0)
+                        ";
+
+                        break;
+                }
+
+
+
+
+                //throw new NotImplementedException();
             }
 
             // Emit the postamble.
@@ -55,7 +115,63 @@ namespace CalcC
         // types are given to you in TokenType.cs.
         private static TokenType GetTokenType(string token)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return Blank;
+            }
+
+            else if (token[0] >= '0' && token[0] <= '9')
+            {
+                return Number;
+            }
+
+            else if (token[0] == '-')
+            {
+                if (token.Length == 1)
+                {
+                    return BinaryOperator;
+                }
+                else
+                {
+                    return Number;
+                }
+            }
+
+            else if (token[0] == '+')
+            {
+                return BinaryOperator;
+            }
+
+            else if (token[0] == '*')
+            {
+                return BinaryOperator;
+            }
+
+            else if (token[0] == '%')
+            {
+                return BinaryOperator;
+            }
+
+            else if (token[0] == '/')
+            {
+                return BinaryOperator;
+            }
+
+            else if (token[0] == 's')
+            {
+                return StoreInstruction;
+            }
+
+            else if (token[0] == 'r')
+            {
+                return RetrieveInstruction;
+            }
+
+            else
+            {
+                return Unknown;
+            }
+            //throw new NotImplementedException();
         }
 
         // Preamble:
